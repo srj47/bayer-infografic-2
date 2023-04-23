5728f0e59959a4651cb2c0e0d21a5c40f033a6b5
srj47 <47615573+srj47@users.noreply.github.com>
2023-04-23 21:30:26 +0530

    stair removed test

--- common/js/utility.js
+++ common/js/utility.js
@@ -1157,7 +1157,7 @@ var utility = {
                     if(index == 14) {    
                         $('.hhtest1').addClass('bounce-in-right');
                         $('.hhtest1').delay(60000).fadeOut();
-                        $('.whdi').delay(60000).fadeIn();
+                        // $('.whdi').delay(60000).fadeIn();
                     }
 
                     /*----------------------------------*/
